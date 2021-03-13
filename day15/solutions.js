ce9f0491e6da339746afda53feb4c0db0abbefe2
Balazs Marjan <marjanbalazs@outlook.com>
2021-03-13 21:54:01 +0100

    day15 working

--- day15/solutions.js
+++ day15/solutions.js
@@ -1,3 +1,31 @@
 const input = require("../input");
 const file = input("input.txt");
+
 const lines = file.split(/[\r\n]/g);
+const numbers = lines[0].split(',');
+
+const turn = (memo, lastNumber, turns) => {
+    if (memo[lastNumber]) {
+        let turnAppearance = memo[lastNumber];
+        memo[lastNumber] = turns;
+        return turns - turnAppearance;
+    } else {
+        memo[lastNumber] = turns;
+        return 0;
+    }
+}
+
+const getAnswer = (numbers, turns) => {
+    turnsCntr = numbers.length + 1;
+    number = 0;
+    const memory = Array(turns).fill(0);
+    numbers.forEach((x, i) => memory[Number.parseInt(x)] = i + 1);
+    while (turnsCntr < turns) {
+        number = turn(memory, number, turnsCntr);
+        turnsCntr += 1;
+    }
+    console.log(number);
+}
+
+getAnswer(numbers, 2020);
+getAnswer(numbers, 30000000);
