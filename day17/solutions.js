779b03a4580457a793a8f8fd2f84ac7f45e91e4a
Balazs Marjan <marjanbalazs@outlook.com>
2021-03-28 11:11:20 +0200

    day17/b working

--- day17/solutions.js
+++ day17/solutions.js
@@ -3,16 +3,16 @@ const file = input("input.txt");
 
 const lines = file.split(/[\r\n]/g);
 
-const state = {
+const STATE = {
   active: true,
   inactive: false,
 };
 
 const initStates = lines.map((line) =>
-  [...line].map((char) => (char === "#" ? state.active : state.inactive))
+  [...line].map((char) => (char === "#" ? STATE.active : STATE.inactive))
 );
 
-const coordinateSystem = {
+const gameOfLife3D = {
   init: function (size) {
     this.points = Array(size)
       .fill()
@@ -21,7 +21,7 @@ const coordinateSystem = {
           .fill()
           .map((e) =>
             Array(size)
-              .fill(state.inactive)
+              .fill(STATE.inactive)
               .map((e) => e)
           )
       );
@@ -45,12 +45,14 @@ const coordinateSystem = {
   play: function () {
     const newPoints = this.points.map((x, xIndex) =>
       x.map((y, yIndex) =>
-        y.map((z, zIndex) => this.getNextState(xIndex, yIndex, zIndex))
+        y.map((state, zIndex) =>
+          this.getNextState(xIndex, yIndex, zIndex, state)
+        )
       )
     );
     this.points = newPoints;
   },
-  getNextState: function (x, y, z) {
+  getNextState: function (x, y, z, state) {
     const getNeighbours = (x, y, z) => {
       const xCoordinates = [x - 1, x, x + 1];
       const yCoordinates = [y - 1, y, y + 1];
@@ -65,17 +67,15 @@ const coordinateSystem = {
     const activeNeighbours = getNeighbours(x, y, z)
       .map(([x, y, z]) => this.points[x]?.[y]?.[z])
       .filter((p) => p)
-      .reduce((acc, point) => (point === state.active ? acc + 1 : acc), 0);
+      .reduce((acc, point) => (point === STATE.active ? acc + 1 : acc), 0);
 
-    const coordinate = coordinateSystem.points[x][y][z];
-
-    return coordinate === state.active
+    return state === STATE.active
       ? activeNeighbours === 2 || activeNeighbours === 3
-        ? state.active
-        : state.inactive
+        ? STATE.active
+        : STATE.inactive
       : activeNeighbours === 3
-      ? state.active
-      : state.inactive;
+      ? STATE.active
+      : STATE.inactive;
   },
   getActivePoints: function () {
     return this.points.reduce(
@@ -84,7 +84,7 @@ const coordinateSystem = {
         x.reduce(
           (accY, y) =>
             accY +
-            y.reduce((accZ, z) => (z === state.active ? accZ + 1 : accZ), 0),
+            y.reduce((accZ, z) => (z === STATE.active ? accZ + 1 : accZ), 0),
           0
         ),
       0
@@ -92,11 +92,116 @@ const coordinateSystem = {
   },
 };
 
-// Game of Life
-coordinateSystem.init(100);
-coordinateSystem.initGame(initStates);
-
+// 3D Game of Life
+gameOfLife3D.init(30);
+gameOfLife3D.initGame(initStates);
 Array(6)
   .fill(0)
-  .forEach(() => coordinateSystem.play());
-console.log(coordinateSystem.getActivePoints());
+  .forEach(() => gameOfLife3D.play());
+console.log(gameOfLife3D.getActivePoints());
+
+// 4D Game of Life
+const gameOfLife4D = {
+  init: function (size) {
+    this.points = Array(size)
+      .fill()
+      .map((e) =>
+        Array(size)
+          .fill()
+          .map((e) =>
+            Array(size)
+              .fill()
+              .map((e) =>
+                Array(size)
+                  .fill(STATE.inactive)
+                  .map((e) => e)
+              )
+          )
+      );
+    this.offset = size / 2;
+  },
+  initGame: function (initPoints) {
+    initPoints.forEach((line, xIndex) =>
+      line.forEach((state, yIndex) => {
+        this.setPoint(
+          xIndex + this.offset,
+          yIndex + this.offset,
+          this.offset,
+          this.offset,
+          state
+        );
+      })
+    );
+  },
+  setPoint: function (x, y, z, w, state) {
+    this.points[x][y][z][w] = state;
+  },
+  play: function () {
+    console.log('Play!')
+    const newPoints = this.points.map((x, xIndex) =>
+      x.map((y, yIndex) =>
+        y.map((z, zIndex) =>
+          z.map((state, wIndex) =>
+            this.getNextState(xIndex, yIndex, zIndex, wIndex, state)
+          )
+        )
+      )
+    );
+    this.points = newPoints;
+  },
+  getNextState: function (x, y, z, w, state) {
+    const getNeighbours = (x, y, z, w) => {
+      const xCoordinates = [x - 1, x, x + 1];
+      const yCoordinates = [y - 1, y, y + 1];
+      const zCoordinates = [z - 1, z, z + 1];
+      const wCoordinates = [w - 1, w, w + 1];
+      const neighbours = xCoordinates
+        .map((x) => yCoordinates.map((y) => zCoordinates.map((z) => wCoordinates.map((w) => [x, y, z, w]))))
+        .flat(3);
+      neighbours.splice(40, 1);
+      return neighbours;
+    };
+
+    const activeNeighbours = getNeighbours(x, y, z, w)
+      .map(([x, y, z, w]) => this.points[x]?.[y]?.[z]?.[w])
+      .filter((p) => p)
+      .reduce((acc, point) => (point === STATE.active ? acc + 1 : acc), 0);
+
+    return state === STATE.active
+      ? activeNeighbours === 2 || activeNeighbours === 3
+        ? STATE.active
+        : STATE.inactive
+      : activeNeighbours === 3
+      ? STATE.active
+      : STATE.inactive;
+  },
+  getActivePoints: function () {
+    return this.points.reduce(
+      (accX, x) =>
+        accX +
+        x.reduce(
+          (accY, y) =>
+            accY +
+            y.reduce(
+              (accZ, z) =>
+                accZ +
+                z.reduce(
+                  (accW, w) => (w === STATE.active ? accW + 1 : accW),
+                  0
+                ),
+              0
+            ),
+          0
+        ),
+      0
+    );
+  },
+};
+
+
+gameOfLife4D.init(30);
+gameOfLife4D.initGame(initStates);
+Array(6)
+  .fill(0)
+  .forEach(() => gameOfLife4D.play());
+console.log(gameOfLife4D.getActivePoints());
