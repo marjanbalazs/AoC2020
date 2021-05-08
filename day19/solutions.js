151bdf7303a1894e1806f71b9ef2d4c06785800a
Balazs Marjan <marjanbalazs@outlook.com>
2021-05-08 19:30:51 +0200

    day19

--- day19/solutions.js
+++ day19/solutions.js
@@ -1,56 +1,27 @@
 const input = require("../input");
 const file = input("input.txt");
 
-const lines = file.split("\r\n");
-console.log(lines);
+var inputAndRule = file.split('\n\n').map(a => a.split('\n'));
+var messages = inputAndRule[1];
+var rules = [];
+inputAndRule[0].forEach(a => rules[a.split(': ')[0]] = a.split(': ')[1]);
+rules = rules.map(a => a.replaceAll('"', ''));
 
-const [rules, inputs] = lines.reduce(
-  (acc, line) => {
-    if (line === "" || acc[1].length > 0) {
-      acc[1].push(line);
-      return acc;
-    }
-    acc[0].push(line);
-    return acc;
-  },
-  [[], []]
-);
+function expandAll(str) {
+  while (str.match(/\d/)) {
+    let strCopy = str.slice()
+    let expList = strCopy.replaceAll(/[\(\)|a-z]/g, '').split(' ').filter(x => !!x);
+    expList = new Set(expList);
+    expList.forEach((exp) => {
+      console.log(str);
+      str = str.replaceAll(new RegExp(`((?<![0-9]))${exp}(?![0-9])`, 'g'), (rules[exp].includes('|') ? (`(${rules[exp]})`) : rules[exp]));
+    });
+  }
+  return str.replaceAll(' ', '');
+}
 
-let rulePrototype = {
-  init: function (line) {
-    const [ruleNum, rest] = line.split(":");
-    const rules = rest.split("|").map((x) => x.trim());
-    this.ruleNum = ruleNum;
-    this.rules = rules;
-  },
-  resolveRule: function (rulesObjects) {
-    if (
-      this.rules.length === 1 &&
-      (this.rules[0] === '"a"' || this.rules[0] === '"b"')
-    ) {
-      this.resolvedRule = this.rules[0].replace(/\"/g, "");
-      return this.resolvedRule;
-    } else {
-      this.resolvedRule = this.rules.map((ruleFragment) =>
-        ruleFragment
-          .split(" ")
-          .map((num) =>
-            rulesObjects[parseInt(num, 10)].resolveRule(rulesObjects)
-          )
-      );
-      return this.resolvedRule;
-    }
-  },
-};
+const rule = expandAll(rules[0]);
+const loopRule = new RegExp(`^${rule}$`);
+console.log(messages.filter(a => loopRule.test(a)).length);
 
-const ruleObjects = rules
-  .map((rule) => {
-    const obj = Object.create(rulePrototype);
-    obj.init(rule);
-    return obj;
-  })
-  .sort((a, b) => a.ruleNum - b.ruleNum);
-
-ruleObjects.forEach((obj) => obj.resolveRule(ruleObjects));
-
-console.dir(ruleObjects, { depth: 5 });
+//https://github.com/eQueline/Advent-of-Code-2020/blob/main/AoC%2019.js
